5f5991a51fa45254ab5f0e9f528084299e5e3ff6
Andrew Peters <amp343@gmail.com>
2014-04-09 14:04:00 -0400

    woo load turk iframes on-demand in chrome

---
+++ js/mt.js
@@ -0,0 +1,35 @@
+if (navigator.appName == 'Microsoft Internet Explorer') {
+	window.frames[0].document.execCommand('Stop');
+}
+else {
+	window.frames[0].stop();
+}
+console.log('iframes stopped... eyyo!');
+
+var element = document.getElementsByTagName("iframe");
+
+for (index = element.length - 1; index >= 0; index--) {
+	var src = element[index].getAttribute('src');
+   	
+    var link = document.createElement('a');
+	link.setAttribute('data-src', src);
+	link.setAttribute('class', 'mt-iframes-btn');
+	link.innerHTML = 'Load Iframe';
+	
+	element[index].parentNode.replaceChild(link, element[index]);
+
+}
+console.log('iframes replaced... boom!');
+
+
+
+jQuery(document).ready(function(){
+
+	jQuery('body').on('click', '.mt-iframes-btn', function(){
+		if(jQuery(this).next('iframe').size() == 0){
+			jQuery(this).after('<iframe></iframe>');
+		}
+		jQuery(this).next('iframe').attr('src', jQuery(this).data('src'));
+	});
+
+});
